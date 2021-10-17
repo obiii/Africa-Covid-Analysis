3f36394d9184cc18ef32cfe63fafdfc37e5f4612
obiii <obaidurrehman1994@gmail.com>
2021-10-17 20:00:18 +0200

    Add table

--- bin/IngestionScript.py
+++ bin/IngestionScript.py
@@ -4,7 +4,7 @@ from google.cloud import storage
 from google.cloud.exceptions import NotFound
 import os, sys, json
 from google.oauth2 import service_account
-
+#sc
 
 project_id = 'covid-africa-analysis'
 dataset_id = 'covidAfrica_stg'
