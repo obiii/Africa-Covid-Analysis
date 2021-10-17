7f26e3739e00b001ae6f68a5c36e394de0a14516
obiii <obaidurrehman1994@gmail.com>
2021-10-17 20:28:00 +0200

    model fix

--- models/covid_mart/covid_africa_stats_v1.sql
+++ models/covid_mart/covid_africa_stats_v1.sql
@@ -3,7 +3,7 @@
 WITH covid_africa_stats as (
 SELECT 
 SUM(Total_Recovered) total_recovered,
-(SELECT Max(median) FROM (SELECT PERCENTILE_CONT(Total_Deaths, 0.5) OVER() AS median from {{ source('covid_africa', 'stg_covidAfrica') }})) as median,
+(SELECT Max(median) FROM (SELECT PERCENTILE_CONT(Total_Deaths, 0.5) OVER() AS median FROM {{ ref('stg_covidAfrica') }} )) as median,
 STDDEV(Total_Tests) std_deviation
 FROM {{ ref('stg_covidAfrica') }} 
 )
